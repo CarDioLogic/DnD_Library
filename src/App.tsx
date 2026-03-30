5206269e4f54357713a6595353a0755acedcf06f
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2026-03-30 12:24:23 +0100

    changing files from jsx to tsx. updaing package-json with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { useState } from 'react';
-import BookView from './views/BookView.jsx';
-import {AllBooks} from './Components/Books/AllBooks.jsx';
+import BookView from './views/BookView.tsx';
+import {AllBooks} from './components/books/AllBooks.tsx';
 
-import MainPageIcons from './components/general/MainPageIcons/MainPageIcons.jsx'
+import MainPageIcons from './components/general/MainPageIcons/MainPageIcons.tsx'
 
 function App() {
   const [currentBook, setCurrentBook] = useState(Object.keys(AllBooks)[0]);
