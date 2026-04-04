5bb8f0b40cb072902b840155a4f399bb47935ea9
João Silva <61765922+CarDioLogic@users.noreply.github.com>
2026-04-04 12:59:03 +0100

    Added attribution

--- src/views/BookView.tsx
+++ src/views/BookView.tsx
@@ -6,6 +6,7 @@ export default function MonstersBookView({ currentBook }) {
   return (
     <div className="wood-bg flex flex-col items-center justify-center bg-gray-400 landscape-only">
       {BookComponent ? <BookComponent /> : <p>Book not found</p>}
+     <a className="text-white mt-2 hover:text-gray-400" href="https://www.dnd5eapi.co/" target="_blank">Powered by D&D 5e API</a>
     </div>
   );
 }
